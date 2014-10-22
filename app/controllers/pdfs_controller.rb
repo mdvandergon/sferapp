cab63f3f8cda8e6f904fe6b0004902ad2989c9d5
mdvandergon <mdvandergon@gmail.com>
2014-10-22 18:25:50 -0500

    Style overhaul of main page
    Added animation on hover, still looking to improve the images styles
    for each resource.

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -1,12 +1,14 @@
 class PdfsController < ApplicationController
+  before_filter :authenticate_user!
 
   def serve
-    path = File.join(Rails.root, 'app', 'pages', params[:filename])
+    path = File.join(Rails.root, 'app', 'views', 'pages', params[:filename])
 
     send_file( path,
     :disposition => 'inline',
     :type => 'application/pdf',
     :x_sendfile => true )
+
   end
 
 end
